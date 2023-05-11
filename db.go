0af9d9691623a27092ebf1c066c1b7a1fdb5cf3b
ADEVA SATRIA ARIF WIBAWA <adeva.wibawa@binus.ac.id>
2023-05-11 14:49:11 +0700

    Menambahkan function delete

--- db.go
+++ db.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	db, err  := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/la_golang")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/la_golang")
 	// if err != nil {
 	// 	panic(err.Error())
 	// }
@@ -19,19 +19,29 @@ func main() {
 	} else {
 		fmt.Println("Successfully connected to the database")
 	}
-	insert(db)
+	// insert(db)
+	delete(db)
 	defer db.Close()
 }
 
-func insert(db *sql.DB) {
-	insert, err := db.Query("INSERT INTO la_user(nim , nama , email ,no_hp)VALUES(2502012464,'adeva','adevawibawa@gmail.com',081280844596)")
-	if err != nil {
+// func insert(db *sql.DB) {
+// 	insert, err := db.Query("INSERT INTO la_user(nim , nama , email ,no_hp)VALUES(2502012464,'adeva','adevawibawa@gmail.com',081280844596)")
+// 	if err != nil {
 
+// 		panic(err.Error())
+
+// 	} else {
+
+// 		insert.Close()
+
+// 	}
+// }
+
+func delete(db *sql.DB) {
+	_, err := db.Exec("DELETE FROM la_user WHERE nim=?", 2502012464)
+	if err != nil {
 		panic(err.Error())
-		
-		} else {
-		
-		insert.Close()
-		
-		}
+	}
+
+	fmt.Println("Delete operation completed successfully.")
 }
